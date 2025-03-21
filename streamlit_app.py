6256f5f56e98c36fcdc6d8bfbc177b5fd5c2e545
Padmajai7 <padmajai.sankar@mrcooper.com>
2025-03-21 09:31:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 name_on_order = st.text_input("Name on smoothie: ")
 st.write("The name on your smoothie will be", name_on_order)
- from snowflake.snowpark.functions import col
+from snowflake.snowpark.functions import col
 #my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
  #ingredients_list = st.multiselect(
